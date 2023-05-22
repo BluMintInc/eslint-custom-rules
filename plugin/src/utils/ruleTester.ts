f4fd0a29d1ed24a45f0d28feb972847278b82c65
brodie-m <brodiedmcguire@gmail.com>
2023-05-22 14:43:54 +0000

    refactor(rule utils): abstract & export

---
+++ plugin/src/utils/ruleTester.ts
@@ -0,0 +1,22 @@
+import { ESLintUtils } from '@typescript-eslint/utils';
+import { RuleTester } from 'eslint';
+
+export const ruleTesterTs = new ESLintUtils.RuleTester({
+  parser: '@typescript-eslint/parser',
+});
+
+export const ruleTesterJsx = new ESLintUtils.RuleTester({
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+});
+
+export const ruleTesterJson = new RuleTester({
+  parser: require.resolve('jsonc-eslint-parser'),
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+});
