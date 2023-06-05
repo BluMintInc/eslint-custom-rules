5a850782dd8489dc95cc98252dff6ef2a6b1ead7
brodie-m <brodiedmcguire@gmail.com>
2023-06-05 09:20:27 +0000

    refactor(always export): rename

--- plugin/src/rules/export-if-in-doubt.ts
+++ plugin/src/rules/export-if-in-doubt.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @blumintinc/blumint/extract-global-constants */
+// import { ASTHelpers } from '../utils/ASTHelpers';
 import { createRule } from '../utils/createRule';
 import { TSESLint, TSESTree } from '@typescript-eslint/utils';
 
 export const exportIfInDoubt: TSESLint.RuleModule<'exportIfInDoubt', never[]> =
   createRule({
-    name: 'always-export',
+    name: 'export-if-in-doubt',
     meta: {
       type: 'suggestion',
       docs: {
@@ -19,21 +21,54 @@ export const exportIfInDoubt: TSESLint.RuleModule<'exportIfInDoubt', never[]> =
     },
     defaultOptions: [],
     create(context) {
+      // List of top-level declarations
+      // List of exported identifiers
+      const topLevelDeclarations: TSESTree.Node[] = [];
+      const exportedIdentifiers: string[] = [];
+
       return {
         'Program > VariableDeclaration > VariableDeclarator, Program > FunctionDeclaration, Program > TSTypeAliasDeclaration'(
           node: TSESTree.Node,
         ) {
-          if (
-            (node.parent && node.parent.type !== 'ExportNamedDeclaration') ||
-            (node.parent &&
-              node.parent.parent &&
-              node.parent.parent.type !== 'ExportNamedDeclaration')
-          ) {
-            context.report({
-              node,
-              messageId: 'exportIfInDoubt',
+          topLevelDeclarations.push(node);
+        },
+        ExportNamedDeclaration: (node: TSESTree.ExportNamedDeclaration) => {
+          if (node.specifiers) {
+            node.specifiers.forEach((specifier) => {
+              if (specifier.type === 'ExportSpecifier') {
+                // Handle both normal and default export
+                const exportedName = specifier.exported.name;
+                if (!exportedIdentifiers.includes(exportedName)) {
+                  exportedIdentifiers.push(exportedName);
+                }
+                // If the specifier is a default export
+                if (specifier.local.name !== exportedName) {
+                  const localName = specifier.local.name;
+                  if (!exportedIdentifiers.includes(localName)) {
+                    exportedIdentifiers.push(localName);
+                  }
+                }
+              }
             });
           }
+        },
+        'Program:exit': () => {
+          topLevelDeclarations.forEach((node) => {
+            if (
+              'id' in node &&
+              node.id &&
+              (node.type === 'VariableDeclarator' ||
+                node.type === 'FunctionDeclaration' ||
+                node.type === 'TSTypeAliasDeclaration') &&
+              node.id.type === 'Identifier' &&
+              !exportedIdentifiers.includes(node.id.name)
+            ) {
+              context.report({
+                node,
+                messageId: 'exportIfInDoubt',
+              });
+            }
+          });
         },
       };
     },
