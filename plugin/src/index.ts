d028bea0c1dbbaa074aa2c478a6778961162564e
brodie-m <brodiedmcguire@gmail.com>
2023-06-21 09:13:05 +0000

    refactor(version): bump to 0.1.13

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -15,7 +15,7 @@ import { preferTypeOverInterface } from './rules/prefer-type-over-interface';
 module.exports = {
   meta: {
     name: '@blumintinc/eslint-plugin-blumint',
-    version: '0.1.12',
+    version: '0.1.13',
   },
   parseOptions: {
     ecmaVersion: 2020,
