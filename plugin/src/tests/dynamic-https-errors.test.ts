cce662951e6087fa499c6f865162919165256b0f
brodie-m <brodiedmcguire@gmail.com>
2023-09-22 10:09:56 +0000

    refactor(dynamic-https-errors): reformat, create docs

--- plugin/src/tests/dynamic-https-errors.test.ts
+++ plugin/src/tests/dynamic-https-errors.test.ts
@@ -1,17 +1,17 @@
 import { ruleTesterTs } from '../utils/ruleTester';
-import {dynamicHttpsErrors} from '../rules/dynamic-https-errors'
+import { dynamicHttpsErrors } from '../rules/dynamic-https-errors';
 
 ruleTesterTs.run('dynamic-https-errors', dynamicHttpsErrors, {
   valid: [
     "throw new https.HttpsError('foo', 'bar', 'baz');",
     "throw new https.HttpsError('foo', 'bar', `Details: ${baz}`);",
-    `throw new HttpsError('foo', 'bar', 'baz');`
+    `throw new HttpsError('foo', 'bar', 'baz');`,
   ],
   invalid: [
     "throw new https.HttpsError('foo', `Error: ${bar}`, 'baz');",
     "https.HttpsError('foo', `Error: ${bar}`, `baz: ${baz}`);",
     "throw https.HttpsError('foo', `Error: ${bar}`, `baz: ${baz}`);",
-    `throw new HttpsError('foo', \`Error: \${bar}\`, 'baz');`
+    `throw new HttpsError('foo', \`Error: \${bar}\`, 'baz');`,
   ].map((testCase) => {
     return {
       code: testCase,
