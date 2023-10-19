6f8c30c96b7a631746bebc52085d654a3aefacef
brodie-m <brodiedmcguire@gmail.com>
2023-10-19 13:23:01 +0000

    build(deps): bump version to 0.1.22

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -17,7 +17,7 @@ import { requireMemo } from './rules/require-memo';
 module.exports = {
   meta: {
     name: '@blumintinc/eslint-plugin-blumint',
-    version: '0.1.21',
+    version: '0.1.22',
   },
   parseOptions: {
     ecmaVersion: 2020,
