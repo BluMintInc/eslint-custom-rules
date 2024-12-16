2002ea14faf9d0d7ef974aec7ac43c8ee804d25f
Joseph O'Connor <oconnor.joseph9@gmail.com>
2024-12-16 05:22:50 -0600

    Delete lib directory

--- lib/utils/graph/ClassGraphSorter.d.ts
+++
@@ -1,7 +0,0 @@
-import { Graph, GraphNode } from './ClassGraphBuilder';
-export declare abstract class ClassGraphSorter {
-    graph: Graph;
-    constructor(graph: Graph);
-    abstract nodesSorted: GraphNode[];
-    abstract nodeNamesSorted: GraphNode['name'][];
-}
