1f545385ea2080947b0e95f7b76ae315408538f3
Joseph O'Connor <oconnor.joseph9@gmail.com>
2025-02-05 15:54:45 -0500

    Add no-complex-cloud-params ESLint rule
    Implement a new ESLint rule to prevent passing complex objects to cloud functions, addressing issue #350

--- src/index.ts
+++ src/index.ts
@@ -52,6 +52,7 @@ import { noMemoizeOnStatic } from './rules/no-memoize-on-static';
 import { noUnsafeFirestoreSpread } from './rules/no-unsafe-firestore-spread';
 import { noJsxInHooks } from './rules/no-jsx-in-hooks';
 import { enforceAssertThrows } from './rules/enforce-assert-throws';
+import { noComplexCloudParams } from './rules/no-complex-cloud-params';
 
 module.exports = {
   meta: {
@@ -119,6 +120,7 @@ module.exports = {
         '@blumintinc/blumint/no-unsafe-firestore-spread': 'error',
         '@blumintinc/blumint/no-jsx-in-hooks': 'error',
         '@blumintinc/blumint/enforce-assert-throws': 'error',
+        '@blumintinc/blumint/no-complex-cloud-params': 'error',
       },
     },
   },
@@ -178,5 +180,6 @@ module.exports = {
     'no-unsafe-firestore-spread': noUnsafeFirestoreSpread,
     'no-jsx-in-hooks': noJsxInHooks,
     'enforce-assert-throws': enforceAssertThrows,
+    'no-complex-cloud-params': noComplexCloudParams,
   },
 };
