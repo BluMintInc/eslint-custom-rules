dbc23371e793fdb359cfbd7ced6ec627333b6a69
Joseph O'Connor <oconnor.joseph9@gmail.com>
2025-02-25 23:40:54 -0500

    chore: remove test-examples directory and related files
    This commit removes the test-examples directory, including its package.json, source files, and test files, which appear to be no longer needed.

--- test-examples/functions/src/util/X.ts
+++
@@ -1,4 +0,0 @@
-// This is a source file
-export function X() {
-  return 'X';
-}
--- test-examples/functions/tests/X.test.ts
+++
@@ -1,8 +0,0 @@
-// This is a test file in the wrong location
-import { X } from '../src/util/X';
-
-describe('X', () => {
-  it('should return X', () => {
-    expect(X()).toBe('X');
-  });
-});
